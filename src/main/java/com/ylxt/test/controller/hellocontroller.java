b4c9eec92190a174e70a3efdec72ea0687f44c4e
zhangdong <1940978083@qq.com>
2019-08-23 09:34:29 +0800

    first commit

---
+++ src/main/java/com/ylxt/test/controller/hellocontroller.java
@@ -0,0 +1,16 @@
+package com.ylxt.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class hellocontroller {
+
+
+    @RequestMapping(value = "docker")
+    @ResponseBody
+    public String hellodocker(){
+        return "hello docker12";
+    }
+}
